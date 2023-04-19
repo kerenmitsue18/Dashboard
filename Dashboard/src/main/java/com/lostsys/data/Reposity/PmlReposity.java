ee09bf672c13cabf0d5961efcc6187e2c0b270b8
Keren <kerenramver@gmail.com>
2023-04-19 09:57:11 -0600

    informacion de nodos completa

--- Dashboard/src/main/java/com/lostsys/data/Reposity/PmlReposity.java
+++ Dashboard/src/main/java/com/lostsys/data/Reposity/PmlReposity.java
@@ -1,18 +1,17 @@
 package com.lostsys.data.Reposity;
 
 import com.lostsys.data.Model.PML;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import javax.persistence.EntityManager;
+
 @Repository("PmlRepo")
 public interface PmlReposity extends JpaRepository<PML, Long> {
 
-    @Query(value = "SELECT id_nodo, ROUND(AVG(precio_marginal),2) AS precio_marginal , " +
-            "ROUND(AVG(comp_energia),2) AS comp_energia, " +
-            "ROUND(AVG(comp_perdidas),2) AS comp_perdidas, " +
-            "ROUND(AVG(comp_congestion),2) AS comp_congestion " +
-            "FROM pml WHERE id_nodo=?1", nativeQuery = true)
-    public PML getPromedio(int id_nodo);
+
+    @Query(value = "SELECT ROUND(AVG(precio_marginal),2) AS precio_marginal, ROUND(AVG(comp_energia),2) AS comp_energia, ROUND(AVG(comp_perdidas),2) AS comp_perdidas, ROUND(AVG(comp_congestion),2) AS comp_congestion FROM pml WHERE id_nodo=?1",nativeQuery = true)
+    public List<Object[]> getPromedio(int id_nodo);
 }
--- Dashboard/src/main/java/com/lostsys/data/Service/api/PmlServiceAPI.java
+++ Dashboard/src/main/java/com/lostsys/data/Service/api/PmlServiceAPI.java
@@ -1,15 +1,28 @@
 package com.lostsys.data.Service.api;
-
 import com.lostsys.data.Model.PML;
 import com.lostsys.data.Reposity.PmlReposity;
+import java.util.List;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 @Service
 public class PmlServiceAPI {
-
     private final PmlReposity pmlReposity;
 
     public PmlServiceAPI(PmlReposity pmlReposity) { this.pmlReposity = pmlReposity;  }
+    public PML getPromedios(int id_nodo){
+        List<Object[]> list = pmlReposity.getPromedio(id_nodo);
+        PML pml = new PML();
+        list.stream().forEach( (record) -> {
+            pml.setPrecio_marginal((Double) record[0]);
+                    pml.setComp_energia((Double) record[1]);
+                    pml.setComp_perdidas((Double) record[2]);
+                    pml.setComp_congestion( (Double) record[3]);
+                });
+        return pml;
 
-    public PML getPromedios(int id_nodo){ return pmlReposity.getPromedio(id_nodo); }
+    }
 }
